6041bceb7e6547391dbaaff221f0491597f32970
Shivam <shivam1@tothenew.com>
2021-02-25 13:20:32 +0530

    Add first project

---
+++ firstmavenproject/src/main/java/firstmavenproject/App.java
@@ -0,0 +1,13 @@
+package firstmavenproject;
+
+public class App {
+	 public static void main( String[] args )
+	    {
+	        System.out.println( "Hello World!" );
+	    }
+
+		public int calculateSomething() {
+			return 0;
+		}
+
+}
